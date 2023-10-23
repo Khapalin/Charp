f3d00643f16ed7ada8e87fce353002b1ca23c067
Eric <elim_14@list.ru>
2023-10-23 12:53:08 +0300

    commit

---
+++ task40/Program.cs
@@ -0,0 +1,46 @@
+﻿/*40. Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины. (a + b > c && a + c > b && b + c > a)*/
+
+Console.Clear();
+
+int UserInOut(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+bool CheckTriangle(int a, int b, int c)
+{
+    if (a + b > c && a + c > b && b + c > a) return true;
+    return false;
+}
+
+
+
+int userA = UserInOut("Введите первое число: ");
+int userB = UserInOut("Введите второе число: ");
+int userC = UserInOut("Введите третье число: ");
+System.Console.WriteLine(CheckTriangle(userA, userB, userC));
+
+/*
+
+Console.Clear();
+
+System.Console.WriteLine("Введите три числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+bool Triangle(int numberA, int numberB, int numberC)
+{
+
+    if (numberA < numberB + numberC && numberB < numberA + numberC && numberC < numberA + numberB)
+    {
+        return true;
+    }
+    return false;
+}
+
+System.Console.WriteLine(Triangle(numberA, numberB, numberC));
+*/
