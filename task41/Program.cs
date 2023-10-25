176ecf7d15e60c2aa96eb2324c6a8b0ac955825a
Eric <elim_14@list.ru>
2023-10-25 14:49:49 +0300

    commit

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,3 +1,75 @@
 ﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+L... — Сегодня, в 13:52
+кинь весь код
+*/
 
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Thread.Sleep(1);
+        System.Console.Write(arr[i]);
+        if (i < arr.Length - 1)
+            System.Console.Write(", ");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.WriteLine("]");
+    Console.ResetColor();
+}
+
+int userSize = Prompt("Enter array size: ");
+int[] userArray = GetArray(userSize);
+PrintArray(userArray);
+
+int CountPositivDigits(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result ++;
+    }
+    return result;
+}
+int count = CountPositivDigits(userArray);
+System.Console.WriteLine();
+System.Console.WriteLine($"Positive Digits > 0 = {count}");
+
+
+/*
+System.Console.Write("Укажите количество чисел: ");
+int value = int.Parse(System.Console.ReadLine());
+
+int count = 0;
+
+for(int i = 0; i < value; i++)
+{
+    System.Console.Write("Введите число: ");
+    if(int.Parse(System.Console.ReadLine()) > 0 ) count++;
+}
+
+System.Console.WriteLine("Чисел со значением больше нуля было введено: " + count);
 */
